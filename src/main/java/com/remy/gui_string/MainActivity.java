448fc23efcadc4849f4cbec2460a29633bd29fc3
remyhai <mihai.voicu97@yahoo.com>
2020-02-10 15:38:56 +0100

    Add files via upload

---
+++ src/main/java/com/remy/gui_string/MainActivity.java
@@ -0,0 +1,38 @@
+package com.remy.gui_string;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+   Button btn;
+   EditText et;
+   String st;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        et = findViewById(R.id.editText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                st = et.getText().toString();
+                i.putExtra("Value",st);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
